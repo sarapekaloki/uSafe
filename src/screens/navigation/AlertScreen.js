56044fc855ec940864e521d9596da5a280e4467c
Angel Almaraz <angel.almaraz@cetys.edu.mx>
2022-10-13 11:46:47 -0700

    button change
    - vibra
    - update on click

--- src/screens/navigation/AlertScreen.js
+++ src/screens/navigation/AlertScreen.js
@@ -1,12 +1,89 @@
 import * as React from 'react';
-import { View, Text} from 'react-native';
+import * as Haptics from 'expo-haptics';
+import {useRef, useState} from "react";
+import { SafeAreaView, View, TouchableOpacity, Text, StyleSheet, Image } from "react-native";
+import BottomSheet from "react-native-gesture-bottom-sheet";
+import image1 from '../../../assets/img/buttonUnpressed.png'
+import image2 from '../../../assets/img/buttonPressed.png'
 
-export default function AlertScreen({navigation}){
-    return(
-        <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-            <Text
-                onPress={()=>alert('This is the "Home" screen')}
-                style={{fontSize:26,fontWeight:'bold'}}>Alert Screen</Text>
-        </View>
-    )
-}
+const AlertScreen = () => {
+    const [image, set_image ] = useState(image1)
+
+    // let alertModeActive = false
+    // const a = require('../../../assets/img/buttonUnpressed.png')
+    // const b = require('../../../assets/img/buttonPressed.png')
+    const bottomSheet = useRef();
+    function changeAlert(){
+        // alertModeActive = !alertModeActive
+        // set_image(alertModeActive ? image2 : image1)
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
+        set_image(image === image1 ? image2 : image1)
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <BottomSheet hasDraggableIcon ref={bottomSheet} height={600} sheetBackgroundColor={"#D4B2EF"}>
+                <View>
+                    <TouchableOpacity
+                        style={styles.button2}
+                        onPress={() => changeAlert()}
+                        >
+                        <Image source={image} style={styles.buttonImage}/>
+                    </TouchableOpacity>
+                </View>
+            </BottomSheet>
+            <TouchableOpacity
+                style={styles.button}
+                onPressIn={() => bottomSheet.current.show()}
+            >
+                <Text style={styles.text}>Open modal</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        height: 50,
+        width: 150,
+        backgroundColor: "#28194C",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        shadowColor: "#D4B2EF",
+        shadowOpacity: 0.7,
+        shadowOffset: {
+            height: 4,
+            width: 4,
+        },
+        shadowRadius: 5,
+        elevation: 6,
+    },
+    button2:{
+        height: 50,
+        width: 150,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        marginTop: 240,
+        marginLeft:'31%'
+    },
+    buttonImage:{
+        width:200,
+        height:200
+    },
+    text: {
+        color: "white",
+        fontWeight: "600",
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+});
+
+export default AlertScreen;
+
+
--- src/screens/navigation/MainContainer.js
+++ src/screens/navigation/MainContainer.js
@@ -8,6 +8,7 @@ import MapScreen from "./MapScreen";
 import ProfileScreen from "./ProfileScreen";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Ionicons from "react-native-vector-icons/Ionicons"
+import BottomSheet from "react-native-gesture-bottom-sheet";
 
 const alertName = 'Alert';
 const settingsName = 'Settings';
@@ -19,41 +20,39 @@ const Stack = createNativeStackNavigator();
 
 export default function MainContainer(){
     return(
-        <NavigationContainer independent={true}>
-            <Tab.Navigator
-            initialRouteName={mapName}
-            screenOptions={({route}) => ({
-                tabBarIcon: ({focused,color,size}) => {
-                    let iconName;
-                    let rn = route.name;
+            <NavigationContainer independent={true}>
+                <Tab.Navigator
 
-                    if(rn === mapName){
-                        iconName = focused ? "globe-outline" : "globe"
-                    } else if (rn === profileName){
-                        iconName = focused ? "person-circle-outline" : "person-circle"
-                    } else if (rn === settingsName){
-                        iconName = focused ? "settings-outline" : "settings"
-                    } else if (rn === alertName){
-                        iconName = focused ? "warning-outline" : "warning"
-                    }
-                    return <Ionicons name={iconName} size={40} color={color}/>
-                },
-                tabBarActiveTintColor: '#28194C',
-                tabBarInactiveTintColor: '#D4B2EF',
-                tabBarShowLabel: false,
-                tabBarStyle: {height:"10%"},
-            })}
-            >
+                    initialRouteName={mapName}
+                    screenOptions={({route}) => ({
+                        tabBarIcon: ({focused,color,size}) => {
+                            let iconName;
+                            let rn = route.name;
 
-                <Tab.Screen options={{ headerShown: false }} name={alertName} component={AlertScreen}/>
-                <Tab.Screen options={{ headerShown: false }} name={settingsName} component={SettingsScreen}/>
-                <Tab.Screen options={{ headerShown: false }} name={profileName} component={ProfileScreen}/>
-                <Tab.Screen options={{ headerShown: false }} name={mapName} component={MapScreen}/>
-
-
+                            if(rn === mapName){
+                                iconName =  focused ? "globe-outline" : "globe"
+                            } else if (rn === profileName){
+                                iconName = focused ? "person-circle-outline" : "person-circle"
+                            } else if (rn === settingsName){
+                                iconName = focused ? "settings-outline" : "settings"
+                            } else if (rn === alertName){
+                                iconName = focused ? "warning-outline" : "warning"
+                            }
+                            return <Ionicons name={iconName} size={40} color={color}/>
+                        },
+                        tabBarActiveTintColor: '#28194C',
+                        tabBarInactiveTintColor: '#D4B2EF',
+                        tabBarShowLabel: false,
+                        tabBarStyle: {height:"10%"},
+                    })}
+                >
+                    <Tab.Screen options={{ headerShown: false }} name={alertName} component={AlertScreen} />
+                    <Tab.Screen options={{ headerShown: false }} name={settingsName} component={SettingsScreen}/>
+                    <Tab.Screen options={{ headerShown: false }} name={profileName} component={ProfileScreen}/>
+                    <Tab.Screen options={{ headerShown: false }} name={mapName} component={MapScreen}/>
 
 
-            </Tab.Navigator>
-        </NavigationContainer>
+                </Tab.Navigator>
+            </NavigationContainer>
     )
 }
