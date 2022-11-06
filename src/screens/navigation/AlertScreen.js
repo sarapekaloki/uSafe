a46d971c8212d27abc63961926f77bef6ad3115e
Angel Almaraz <angel.almaraz@cetys.edu.mx>
2022-11-05 19:01:17 -0700

    notruenaaun
    uwu

--- src/screens/navigation/AlertScreen.js
+++ src/screens/navigation/AlertScreen.js
@@ -8,6 +8,12 @@ import image1 from '../../../assets/img/buttonUnpressed.png'
 import image2 from '../../../assets/img/buttonPressed2.png'
 
 export default function AlertScreen({navigation}){
+    const [image, set_image ] = useState(image1)
+    const bottomSheet = useRef();
+    function changeAlert(){
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
+        set_image(image === image1 ? image2 : image1);}
+
     return(
         <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
             <BottomSheet hasDraggableIcon ref={bottomSheet} height={600} sheetBackgroundColor={"#D4B2EF"}>
@@ -23,3 +29,45 @@ export default function AlertScreen({navigation}){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        height: 50,
+        width: 150,
+        backgroundColor: "#28194C",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        shadowColor: "#D4B2EF",
+        shadowOpacity: 0.7,
+        shadowOffset: {
+            height: 4,
+            width: 4,
+        },
+        shadowRadius: 5,
+        elevation: 6,
+    },
+    button2:{
+        height: 50,
+        width: 150,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        marginTop: 240,
+        marginLeft:'31%'
+    },
+    buttonImage:{
+        width:'154%',
+        height:'460%'
+    },
+    text: {
+        color: "white",
+        fontWeight: "600",
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+});
