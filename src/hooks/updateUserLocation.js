5fce2d5315383342a149038886cee03b1102c0f6
sarapekaloki <sara.marquez@cetys.edu.mx>
2022-11-07 11:43:40 -0800

    Error mapa al cerrar sesión

--- src/hooks/updateUserLocation.js
+++ src/hooks/updateUserLocation.js
@@ -4,22 +4,24 @@ import {auth} from "../../firebase";
 
 export const updateUserLocation = async (userLocation, setUserLocation, currentUser) => {
     let current_user_ref;
-    const db = getFirestore();
-    const usersRef = collection(db, "users2");
-    await getDocs(usersRef).then((res) => {
-        res.forEach((doc) => {
-            if((doc.data().email).toLowerCase() === auth.currentUser.email){
-                current_user_ref= doc.data()
-            }
+    if(current_user_ref){
+        const db = getFirestore();
+        const usersRef = collection(db, "users2");
+        await getDocs(usersRef).then((res) => {
+            res.forEach((doc) => {
+                if((doc.data().email).toLowerCase() === auth.currentUser.email){
+                    current_user_ref= doc.data()
+                }
+            })
         })
-    })
-    getUserLocation(setUserLocation).then();
-    const userRef = doc(db, "users2", current_user_ref.email);
-    updateDoc(userRef, {
-        coordinates:userLocation,
-        email:current_user_ref.email,
-        helpResponses:current_user_ref.helpResponses,
-        pictureUrl:current_user_ref.pictureUrl,
-        username:current_user_ref.username
-    }).then();
+        getUserLocation(setUserLocation).then();
+        const userRef = doc(db, "users2", current_user_ref.email);
+        updateDoc(userRef, {
+            coordinates:userLocation,
+            email:current_user_ref.email,
+            helpResponses:current_user_ref.helpResponses,
+            pictureUrl:current_user_ref.pictureUrl,
+            username:current_user_ref.username
+        }).then();
+    }
 }
