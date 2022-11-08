43d0e2bc29d8bea4c464ba6d645d116356e19568
sarapekaloki <sara.marquez@cetys.edu.mx>
2022-11-07 19:12:38 -0800

    hola

--- src/hooks/updateUserLocation.js
+++ src/hooks/updateUserLocation.js
@@ -16,7 +16,7 @@ export const updateUserLocation = async (userLocation, currentUser, setCurrentUs
     }
 
     if(current.latitude && current.longitude){
-        if(auth){
+        if(auth && auth.currentUser && auth.currentUser.email){
             const userRef = doc(db, "users2", auth.currentUser.email);
             const userObject = {
                 coordinates:current,
